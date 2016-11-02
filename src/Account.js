b96f89adb7423964041f794ebcafc83002af3d9c
jordan wade <jordanwade2009@live.com>
2016-11-02 07:29:44 -0500

    hw

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor(){
+    super();
+    this.state = {
+      balance: 0
+    }
+  }
+  _deposit() {
+    let input = this.refs.moneyField.value;
+    this.setState({
+      balance: this.state.balance + +input
+    })
+  }
+  _withdraw() {
+    let input = this.refs.moneyField.value;
+    if (this.state.balance > input ) {
+      this.setState({
+        balance: this.state.balance - input
+      })
+    } else {
+      alert('insufficient funds')
+    }
+  }
+
   render() {
+    if (this.state.balance === 0) {
+      //  this.setState({className: 'zero'});
+    }
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance" >${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref="moneyField" />
+        <input type="button" value="Deposit" onClick={() => this._deposit()} />
+        <input type="button" value="Withdraw" onClick={() => this._withdraw()} />
       </div>
     )
   }
